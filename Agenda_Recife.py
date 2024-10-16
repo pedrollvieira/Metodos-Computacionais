843e015b40069f6e0a2b9983a3692f342fe1f50a
pedrollvieira <pedro.llvieira@ufpe.br>
2024-10-16 15:28:00 -0300

    Correções de cálculos

--- Agenda_Recife.py
+++ Agenda_Recife.py
@@ -22,36 +22,28 @@ def merge_sort(lista):
     if len(lista) <= 1:
         return lista
 
-    # Encontrando o meio da lista e dividindo-a em duas partes
     meio = len(lista) // 2
     lado_esquerdo = lista[:meio]
     lado_direito = lista[meio:]
 
-    # Chamando a função recursivamente para dividir as partes restantes
     lado_esquerdo = merge_sort(lado_esquerdo)
     lado_direito = merge_sort(lado_direito)
 
-    # Mesclando as duas partes ordenadas
     return merge(lado_esquerdo, lado_direito)
 
 # Função para mesclar as duas metades da lista e ordenar segundo a data/hora
 def merge(lado_esquerdo, lado_direito):
-    # Lista para armazenar a lista_ordenada
     lista_ordenada = []
     i = j = 0
 
-    # Comparando os elementos de cada lista
     while i < len(lado_esquerdo) and j < len(lado_direito):
-        # Comparando as datas/horas dos eventos. Se a data/hora do lado esquerdo for menor, adicionar o evento a lista_ordenada
         if comparar_data_hora(lado_esquerdo[i], lado_direito[j]) <= 0:
             lista_ordenada.append(lado_esquerdo[i])
             i += 1
-        # Caso contrário, adicionar o evento do lado direito ao lista_ordenada
         else:
             lista_ordenada.append(lado_direito[j])
             j += 1
 
-    # Adicionando os elementos restantes de cada lista
     lista_ordenada.extend(lado_esquerdo[i:])
     lista_ordenada.extend(lado_direito[j:])
     return lista_ordenada
@@ -61,16 +53,9 @@ def quicksort(lista):
     if len(lista) <= 1:
         return lista
     
-    # Escolhendo o pivot como o elemento do meio da lista
     pivot = lista[len(lista) // 2]
     
-    # Função para dividir a lista em três partes (esquerda, meio e direita) em relação ao pivot
     def dividir_lista(lista, pivot):
-        '''
-        esquerda: eventos com data/hora menor que o pivot
-        meio: eventos com data/hora igual ao pivot
-        direita: eventos com data/hora maior que o pivot
-        '''
         esquerda = [x for x in lista if comparar_data_hora(x, pivot) < 0]
         meio = [x for x in lista if comparar_data_hora(x, pivot) == 0]
         direita = [x for x in lista if comparar_data_hora(x, pivot) > 0]
@@ -78,67 +63,50 @@ def quicksort(lista):
 
     esquerda, meio, direita = dividir_lista(lista, pivot)
 
-    # Chamando a função recursivamente para ordenar as partes esquerda e direita
     return quicksort(esquerda) + meio + quicksort(direita)
 
 # Função para ordenar a lista de eventos usando Heapsort
 def heap_sort(lista):
-    # Função para acessar o elemento no índice ajustado
     def posicao(lista, indice):
-        return lista[indice - 1]  # indexação ajustada para começar em 1
+        return lista[indice - 1]
 
-    # Função para trocar elementos na lista
     def troca(lista, indice1, indice2):
         if indice1 > len(lista) or indice2 > len(lista):
             return
         lista[indice1 - 1], lista[indice2 - 1] = lista[indice2 - 1], lista[indice1 - 1]
 
-    # Função para garantir que a propriedade do heap máximo seja mantida
     def max_heapify(lista, tamanho, pai):
-        esquerda = 2 * pai  # índice do filho esquerdo
-        direita = 2 * pai + 1  # índice do filho direito
-        maior = pai  # inicializa maior com índice do pai
+        esquerda = 2 * pai
+        direita = 2 * pai + 1
+        maior = pai
 
-        # Verificar se o filho esquerdo é maior que o pai
         if esquerda <= tamanho and comparar_data_hora(posicao(lista, esquerda), posicao(lista, pai)) > 0:
             maior = esquerda
 
-        # Verificar se o filho direito é maior que o pai
         if direita <= tamanho and comparar_data_hora(posicao(lista, direita), posicao(lista, maior)) > 0:
             maior = direita
 
-        # Se o maior elemento não é o pai, trocar as posições
         if maior != pai:
             troca(lista, maior, pai)
-            max_heapify(lista, tamanho, maior)  # Garantir que a regra se repita na subárvore
+            max_heapify(lista, tamanho, maior)
 
-    # Função para construir o heap máximo
     def construir_heap(tamanho, lista):
-        inicio = tamanho // 2  # calcula o índice do último nó pai
+        inicio = tamanho // 2
         for i in range(inicio, 0, -1):
             max_heapify(lista, tamanho, i)
 
-    # Construir o heap máximo
     tamanho = len(lista)
     construir_heap(tamanho, lista)
 
-    # Extrair elementos do heap um por um
     for i in range(tamanho, 1, -1):
         troca(lista, 1, i)
         tamanho -= 1
-        max_heapify(lista, tamanho, 1)  # Chamar max_heapify na raiz para ajustar o heap
+        max_heapify(lista, tamanho, 1)
 
     return lista
 
 # Função para adicionar novos eventos à lista
 def adicionar_evento(lista):
-    '''
-    nome = input('Digite o nome do evento: ')
-    dia = input('Digite o dia do evento: ')
-    hora = input('Digite a hora do evento: ')
-    local = input('Digite o local do evento: ')
-    evento = {'nome': nome, 'data': dia, 'hora': hora, 'local': local}
-    '''
     novos_eventos = [
         {'nome': 'Olha! Recife Noturno:', 'data': '01/10/2024', 'hora': '21:00', 'local': 'Tour Histórico'},
         {'nome': 'Olha! Recife Pedalando:', 'data': '29/09/2024', 'hora': '09:00', 'local': 'Antigos Cinemas do Recife'},
@@ -146,43 +114,6 @@ def adicionar_evento(lista):
     ]
     
     lista.extend(novos_eventos)
-    return lista
-
-# Função para remover eventos da lista
-def remover_evento(lista):
-    '''
-    nome = input('Digite o nome do evento que deseja remover: ')
-    dia = input('Digite o dia do evento que deseja remover: ')
-    hora = input('Digite a hora do evento que deseja remover: ')
-    local = input('Digite o local do evento que deseja remover: ')
-    evento = {'nome': nome, 'data': dia, 'hora': hora, 'local': local}
-    lista.remove(evento)
-    '''
-    return lista
-    
-
-# Função para alterar data/hora de um evento
-def alterar_evento(lista):
-    '''
-    evento_antigo = input('Digite o nome, o dia, a hora e o local do evento que deseja alterar (formato: nome: dd/mm/aaaa hh:mm local): ')
-
-    novo_dia = input('Digite o novo dia do evento (formato: dd/mm/aaaa): ')
-    nova_hora = input('Digite a nova hora do evento (formato: hh:mm): ')
-    novo_local = input('Digite o novo local do evento: ')
-
-    for evento in lista:
-        # Verifica se o nome, dia e hora do evento correspondem ao evento antigo
-        if evento['nome'] + ': ' + evento['data'] + ' ' + evento['hora'] + ' ' + evento['local'] == evento_antigo:
-            # Atualiza os detalhes do evento
-            evento['data'] = novo_dia
-            evento['hora'] = nova_hora
-            evento['local'] = novo_local
-            print('Evento alterado com sucesso!')
-            return lista
-
-    # Se o evento não for encontrado, informa ao usuário
-    print('Evento não encontrado.')
-    '''
     return lista
 
 # Função para identificar o método de ordenação a ser utilizado
@@ -196,19 +127,21 @@ def metodo_ordenacao(lista, metodo=''):
 
 # Função para identificar se houve mudanças na lista inicial de eventos
 def houve_mudanca(lista_inicial, lista_atual):
-    # Verificar o comprimento das duas listas
     if len(lista_inicial) != len(lista_atual):
         return True
     
-    # Caso os tamanhos forem iguais, comparar cada evento das duas listas
     for evento_inicial, evento_atual in zip(lista_inicial, lista_atual):
         if evento_inicial != evento_atual:
             return True
     
     return False
 
+# Função para comparar os tempos de execução e imprimir o melhor
+def comparar_tempos(tempos):
+    melhor_metodo = min(tempos, key=tempos.get)
+    print(f'O melhor método é {melhor_metodo} com tempo de execução de {tempos[melhor_metodo]:.10f} segundos')
+
 def main():
-    # Lista de dicionários para cada evento inicial
     lista_inicial = [
         {'nome': 'Olha! Recife a Pé:', 'data': '27/09/2024', 'hora': '09:30', 'local': 'Recife Walking Tour'},
         {'nome': 'Olha! Recife no Rio:', 'data': '28/09/2024', 'hora': '09:00', 'local': 'Ilha de Deus'},
@@ -220,34 +153,50 @@ def main():
         {'nome': 'Olha! Recife a Pé:', 'data': '04/10/2024', 'hora': '09:30', 'local': 'Recife Walking Tour'},
     ]
     
-    # Ordenar a lista inicial usando Quick Sort
-    lista_inicial_ordenada = metodo_ordenacao(lista_inicial, 'mergesort')
+    # Ordenar a lista inicial com cada método
+    lista_mergesort = metodo_ordenacao(copy.deepcopy(lista_inicial), 'mergesort')
+    lista_quicksort = metodo_ordenacao(copy.deepcopy(lista_inicial), 'quicksort')
+    lista_heapsort = metodo_ordenacao(copy.deepcopy(lista_inicial), 'heapsort')
 
-    # Fazer uma cópia da lista inicial para evitar alterações indesejadas
-    copia_lista = copy.deepcopy(lista_inicial_ordenada)
-    
-    # Adicionar novos eventos
-    lista_nova = adicionar_evento(lista_inicial_ordenada)
+    # Adicionar novos eventos à lista ordenada
+    lista_mergesort = adicionar_evento(lista_mergesort)
+    lista_quicksort = adicionar_evento(lista_quicksort)
+    lista_heapsort = adicionar_evento(lista_heapsort)
 
-    # Verificar se houve mudanças na lista
-    if houve_mudanca(copia_lista, lista_nova):
-        # Medir o tempo de execução de cada algoritmo
-        start_time = time.time()
-        lista_nova_merge = metodo_ordenacao(lista_nova, 'mergesort')
-        tempo_merge = time.time() - start_time
+    # Reordenar a lista com cada método e medir o tempo de execução
+    tempos = {}
 
-        start_time = time.time()
-        lista_nova_quick = metodo_ordenacao(lista_nova, 'quicksort')
-        tempo_quick = time.time() - start_time
+    start_time = time.time()
+    lista_nova_merge = metodo_ordenacao(lista_mergesort, 'mergesort')
+    tempos['mergesort'] = time.time() - start_time
 
-        start_time = time.time()
-        lista_nova_heap = metodo_ordenacao(lista_nova, 'heapsort')
-        tempo_heap = time.time() - start_time
+    start_time = time.time()
+    lista_nova_quick = metodo_ordenacao(lista_quicksort, 'quicksort')
+    tempos['quicksort'] = time.time() - start_time
 
-        # Exibir os resultados
-        print('Tempo de execução: ', "--- %.10f segundos ---" % tempo_merge)
-        print('Tempo de execução: ', "--- %.10f segundos ---" % tempo_quick)
-        print('Tempo de execução: ', "--- %.10f segundos ---" % tempo_heap)
+    start_time = time.time()
+    lista_nova_heap = metodo_ordenacao(lista_heapsort, 'heapsort')
+    tempos['heapsort'] = time.time() - start_time
+
+    print("\nLista ordenada com Merge Sort:")
+    for evento in lista_nova_merge:
+        print(evento)
+
+    print("\nLista ordenada com Quick Sort:")
+    for evento in lista_nova_quick:
+        print(evento)
+
+    print("\nLista ordenada com Heap Sort:")
+    for evento in lista_nova_heap:
+        print(evento)
+
+    print("\n")
+    print(f"Tempo de execução do Merge Sort: {tempos['mergesort']:.6f} segundos")
+    print(f"Tempo de execução do Quick Sort: {tempos['quicksort']:.6f} segundos")
+    print(f"Tempo de execução do Heap Sort: {tempos['heapsort']:.6f} segundos")
+
+    # Comparar os tempos de execução e imprimir o melhor
+    comparar_tempos(tempos)
 
 if __name__ == "__main__":
     main()
--- Metro_Recife.py
+++ Metro_Recife.py
@@ -5,34 +5,34 @@ from collections import deque
 # Função Dijkstra
 def dijkstra_adj(graph, start):
     n = len(graph)
-    distancias = [float('infinity')] * n   # inicialmente todas as distâncias são infinitas
-    distancias[start] = 0                  # distância do nó para ele mesmo (início) é nula
-    pq = [(0, start)]                      # fila de prioridade que é inicializada com o primeiro nó
+    distancias = [float('infinity')] * n
+    distancias[start] = 0
+    pq = [(0, start)]
 
-    while pq:                              # o loop continua enquanto a lista de prioridade não estiver vazia
-        dAtual, no_atual = heapq.heappop(pq) # remove e retorna o nó com menor distância da lista de prioridade
+    while pq:
+        dAtual, no_atual = heapq.heappop(pq)
         if dAtual > distancias[no_atual]:
             continue
         for vizinho, tempo in graph[no_atual]:
             distancia = dAtual + tempo
             if distancia < distancias[vizinho]:
                 distancias[vizinho] = distancia
-                heapq.heappush(pq, (distancia, vizinho)) # adiciona o nó vizinho à lista de prioridades
+                heapq.heappush(pq, (distancia, vizinho))
     return distancias
 
 # Função BFS
 def bfs(graph, inicio):
     n = len(graph)
-    distancias = [float('infinity')] * n # definindo as distâncias para os outros nós como infinito
-    distancias[inicio] = 0               # distância do nó para ele mesmo sendo 0
-    fila = deque([inicio])               # inserindo o nó inicial em uma fila
+    distancias = [float('infinity')] * n
+    distancias[inicio] = 0
+    fila = deque([inicio])
 
-    while fila:                          # enquanto a fila tiver algum elemento
-        node = fila.popleft()            # remove o primeiro nó da fila
+    while fila:
+        node = fila.popleft()
         for vizinho, tempo in graph[node]:
-            if distancias[vizinho] == float('infinity'): # se o nó ainda não tiver sido visitado, a distância vai ser infinita
-                distancias[vizinho] = distancias[node] + tempo # atualiza a distância do vizinho
-                fila.append(vizinho)    # adiciona o vizinho à fila e continua o loop até todos os nós serem descobertos
+            if distancias[vizinho] == float('infinity'):
+                distancias[vizinho] = distancias[node] + tempo
+                fila.append(vizinho)
     return distancias
 
 # Função DFS
@@ -40,13 +40,13 @@ def dfs(graph, inicio):
     n = len(graph)
     distancias = [float('infinity')] * n
     distancias[inicio] = 0
-    stack = [(inicio, 0)] # aqui cria uma pilha com o nó inicial
+    stack = [(inicio, 0)]
 
-    while stack:                 # enquanto a pilha tiver algum nó
-        no, dAtual = stack.pop() # remove o último nó da pilha
-        for vizinho, tempo in graph[no]: # pra cada vizinho e tempo, vai atualizar a distância com a do próximo vizinho
+    while stack:
+        no, dAtual = stack.pop()
+        for vizinho, tempo in graph[no]:
             distancia = dAtual + tempo
-            if distancia < distancias[vizinho]: # se a próxima distância for menor do que a registrada, atualiza a distância e adiciona o vizinho à pilha
+            if distancia < distancias[vizinho]:
                 distancias[vizinho] = distancia
                 stack.append((vizinho, distancia))
     return distancias
@@ -87,19 +87,57 @@ no_inicial = 0
 no_final = 19
 
 # Calcular menor tempo usando Dijkstra
+# Calcular menor tempo usando Dijkstra
 tempo_inicio = time.time()
 menortempo_dijkstra = dijkstra_adj(graph_adj, no_inicial)
-print(f"Menor tempo de viagem de '{estacoes[no_inicial]}' para '{estacoes[no_final]}' por Dijkstra: {menortempo_dijkstra[no_final]} minutos")
-print('tempo de execução: ', "--- %.7f segundos ---" % (time.time() - tempo_inicio))
+tempo_dijkstra = time.time() - tempo_inicio
 
 # Calcular menor tempo usando BFS
 tempo_inicio = time.time()
 menortempo_bfs = bfs(graph_adj, no_inicial)
-print(f"Menor tempo de viagem de '{estacoes[no_inicial]}' para '{estacoes[no_final]}' por BFS: {menortempo_bfs[no_final]} minutos")
-print('tempo de execução: ', "--- %.7f segundos ---" % (time.time() - tempo_inicio))
+tempo_bfs = time.time() - tempo_inicio
 
 # Calcular menor tempo usando DFS
 tempo_inicio = time.time()
 menortempo_dfs = dfs(graph_adj, no_inicial)
-print(f"Menor tempo de viagem de '{estacoes[no_inicial]}' para '{estacoes[no_final]}' por DFS: {menortempo_dfs[no_final]} minutos")
-print('tempo de execução: ', "--- %.7f segundos ---" % (time.time() - tempo_inicio))
+tempo_dfs = time.time() - tempo_inicio
+
+menor_tempo = min(menortempo_dijkstra[no_final], menortempo_bfs[no_final], menortempo_dfs[no_final])
+
+if menor_tempo == menortempo_dijkstra[no_final]:
+    algoritmo_eficiente = "Dijkstra"
+    tempo_execucao = tempo_dijkstra
+elif menor_tempo == menortempo_bfs[no_final]:
+    algoritmo_eficiente = "BFS"
+    tempo_execucao = tempo_bfs
+else:
+    algoritmo_eficiente = "DFS"
+    tempo_execucao = tempo_dfs
+
+print(f"O algoritmo mais eficiente é {algoritmo_eficiente} com tempo de viagem de {menor_tempo} minutos")
+#print('Tempo de execução: ', "--- %.10f segundos ---" % tempo_execucao)
+
+# Proposta de nova conexão entre Curado (3) e Prazeres (18) com um tempo estimado de 8 minutos
+print("\nCom uma proposta de nova conexão entre Curado e Prazeres, com tempo estimado de 8 minutos, obtemos:")
+graph_adj[3].append((18, 8))
+graph_adj[18].append((3, 8))
+
+# Recalcular menor tempo usando o algoritmo mais eficiente
+if algoritmo_eficiente == "Dijkstra":
+    tempo_inicio = time.time()
+    menortempo_dijkstra = dijkstra_adj(graph_adj, no_inicial)
+    tempo_execucao = time.time() - tempo_inicio
+    menor_tempo = menortempo_dijkstra[no_final]
+elif algoritmo_eficiente == "BFS":
+    tempo_inicio = time.time()
+    menortempo_bfs = bfs(graph_adj, no_inicial)
+    tempo_execucao = time.time() - tempo_inicio
+    menor_tempo = menortempo_bfs[no_final]
+else:
+    tempo_inicio = time.time()
+    menortempo_dfs = dfs(graph_adj, no_inicial)
+    tempo_execucao = time.time() - tempo_inicio
+    menor_tempo = menortempo_dfs[no_final]
+
+print(f"Algoritmo mais eficiente é {algoritmo_eficiente} com tempo de viagem de {menor_tempo} minutos")
+#print('Tempo de execução: ', "--- %.10f segundos ---" % tempo_execucao)
